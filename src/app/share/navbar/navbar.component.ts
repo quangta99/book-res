77dfc68113a3d3f139ee37a635c719b8df82647b
quangta99 <txqsevena@gmail.com>
2020-11-21 14:38:24 +0700

    update

---
+++ src/app/share/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { DemoserviceService } from './../../services/demoservice.service';
+import { TagPlaceholder } from '@angular/compiler/src/i18n/i18n_ast';
+import { Component, OnInit } from '@angular/core';
+import {ConfirmationService} from 'primeng/api';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+
+export class NavbarComponent implements OnInit {
+  cities: ['TP.HCM','HN'];
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+}
